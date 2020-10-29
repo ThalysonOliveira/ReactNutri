f4b511ab0530a73096d7794abd150fadb021ada2
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-29 17:07:16 -0200

    Criado componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      nutri: []
+    }
+  }
+
+  async componentDidMount(){
+    const url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+    const reponse = await fetch(url)
+    const apiNutri = await reponse.json()
   }
 
   render(){
